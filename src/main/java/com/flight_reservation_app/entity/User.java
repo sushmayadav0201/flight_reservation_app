0c2da0ae3e44c83baa3891052eb5bddb5be8708c
Sushma Kumari <sushmayadav0201@gmail.com>
2023-06-11 16:43:25 +0530

    created User class in entity

---
+++ src/main/java/com/flight_reservation_app/entity/User.java
@@ -0,0 +1,23 @@
+package com.flight_reservation_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
